cdb792d3fe01912c238c8fa823d4002b3f6035a3
slowrunr <mungaslov.nft@gmail.com>
2023-11-13 22:03:20 +0900

    init

--- controller.js
+++ controller.js
@@ -22,11 +22,6 @@ export class Controller {
 
     this.model.setCurrentMemeId(memes[0].id);
 
-    // this.view.renderMemesSelect(
-    //   this.model.getMemes(),
-    //   this.model.getCurrentMemeId()
-    // );
-
     // const preview = this.model.getPreview();
     // this.view.renderPreview(preview);
   }
